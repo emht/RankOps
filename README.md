1625af9101020682c3d42080f21e6afb7233a28d
emht <emht@users.noreply.github.com>
2017-11-14 00:09:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Rank Ops
+========
+
+It is a python based utility which can be used to rank certain number of github profiles to find the best of some certain profiles.
+
+Usage:
+=====
+
+
+Applications:
+============
+
+Drawbacks:
+==========
---
+++ collect_info.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+class collect_info(file_name):
+	regex = re.compile(
+		
+	
+	def file_read(file_name):
+		fp = open(file_name)
+		return(contents = fp.read())
+
+	def collect_handles(file_name):
+	
+	def collect_user_name(file_name):
+		
+		
